e8492a850f8b3bc6a8fa5fe2f60be9e7456a3d86
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2025-03-14 18:46:05 +0200

    Update linux-terraform-installer.sh

--- linux-terraform-installer.sh
+++ linux-terraform-installer.sh
@@ -16,16 +16,32 @@ is_wsl() {
     esac
 }
 
-is_darwin() {
-    case "$(uname -s)" in
-    *darwin* ) true ;;
-    *Darwin* ) true ;;
-    * ) false;;
-    esac
-}
-
 command_exists() {
   command -v "$@" > /dev/null 2>&1
+}
+
+install_lsb_release() {
+    if command_exists sudo; then
+        sudo apt-get update && sudo apt-get install -y lsb-release
+    else
+        apt-get update && apt-get install -y lsb-release
+    fi
+}
+if ! command_exists lsb_release; then
+    install_lsb_release
+fi
+
+get_upstream_distro() {
+    if command_exists lsb_release; then
+        upstream_distro=$(lsb_release -is 2>/dev/null)
+        if [ -n "$upstream_distro" ]; then
+            echo "$upstream_distro"
+        else
+            echo "$lsb_dist"
+        fi
+    else
+        echo "$lsb_dist"
+    fi
 }
 
 lsb_dist=$( get_distribution )
@@ -33,12 +49,10 @@ lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
 if is_wsl; then
     echo "Operating System: Windows Subsystem for Linux (WSL)"
-elif is_darwin; then
-    echo "Operating System: macOS"
 else
     dist_version=""
     case "$lsb_dist" in
-        ubuntu|debian)
+        ubuntu|debian|linuxmint)
             if command_exists lsb_release; then
                 dist_version="$(lsb_release --codename | cut -f2)"
             fi
@@ -74,11 +88,12 @@ else
     esac
 fi
 
-# Terraform Installation
 echo "Installing Terraform..."
 
-case "$lsb_dist" in
-    ubuntu|debian)
+upstream_distro=$(get_upstream_distro)
+
+case "$upstream_distro" in
+    Ubuntu|Debian)
         if command_exists sudo; then
             sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
             wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
@@ -87,11 +102,11 @@ case "$lsb_dist" in
         else
             apt-get update && apt-get install -y gnupg software-properties-common
             wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-            echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+            echo "deb [signed-by=/usr/share/keyrings/hashicorp.archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
             apt-get update && apt-get install -y terraform
         fi
-    ;;
-    centos|amazon)
+        ;;
+    CentOS|RHEL|Fedora|AmazonLinux)
         if command_exists sudo; then
             sudo yum install -y yum-utils
             sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
@@ -101,22 +116,11 @@ case "$lsb_dist" in
             yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
             yum -y install terraform
         fi
-    ;;
-    fedora)
-        if command_exists sudo; then
-            sudo dnf install -y dnf-plugins-core
-            sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-            sudo dnf -y install terraform
-        else
-            dnf install -y dnf-plugins-core
-            dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-            dnf -y install terraform
-        fi
-    ;;
+        ;;
     *)
         echo "Terraform installation is not supported on this distribution."
         exit 1
-    ;;
+        ;;
 esac
 
 if command_exists terraform; then
