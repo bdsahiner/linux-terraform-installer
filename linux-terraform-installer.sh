a6a020fb8b77ed2f0042bda2c2e685bf1062d22b
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2025-03-14 18:49:28 +0200

    Update linux-terraform-installer.sh

--- linux-terraform-installer.sh
+++ linux-terraform-installer.sh
@@ -8,92 +8,96 @@ get_distribution() {
     echo "$lsb_dist"
 }
 
-is_wsl() {
-    case "$(uname -r)" in
-    *microsoft* ) true ;;
-    *Microsoft* ) true ;;
-    * ) false;;
-    esac
-}
-
 command_exists() {
   command -v "$@" > /dev/null 2>&1
 }
 
-install_lsb_release() {
-    if command_exists sudo; then
-        sudo apt-get update && sudo apt-get install -y lsb-release
-    else
-        apt-get update && apt-get install -y lsb-release
-    fi
-}
-if ! command_exists lsb_release; then
-    install_lsb_release
-fi
-
-get_upstream_distro() {
+check_forked() {
     if command_exists lsb_release; then
-        upstream_distro=$(lsb_release -is 2>/dev/null)
-        if [ -n "$upstream_distro" ]; then
-            echo "$upstream_distro"
+        set +e
+        lsb_release -a -u > /dev/null 2>&1
+        lsb_release_exit_code=$?
+        set -e
+
+        if [ "$lsb_release_exit_code" = "0" ]; then
+            lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[:space:]')
+            dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[:space:]')
         else
-            echo "$lsb_dist"
+            if [ -r /etc/debian_version ] && [ "$lsb_dist" != "ubuntu" ] && [ "$lsb_dist" != "raspbian" ]; then
+                if [ "$lsb_dist" = "osmc" ]; then
+                    lsb_dist=raspbian
+                else
+                    lsb_dist=debian
+                fi
+                dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+                case "$dist_version" in
+                    12)
+                        dist_version="bookworm"
+                    ;;
+                    11)
+                        dist_version="bullseye"
+                    ;;
+                    10)
+                        dist_version="buster"
+                    ;;
+                    9)
+                        dist_version="stretch"
+                    ;;
+                    8)
+                        dist_version="jessie"
+                    ;;
+                esac
+            fi
         fi
-    else
-        echo "$lsb_dist"
     fi
 }
 
 lsb_dist=$( get_distribution )
 lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
-if is_wsl; then
-    echo "Operating System: Windows Subsystem for Linux (WSL)"
-else
-    dist_version=""
-    case "$lsb_dist" in
-        ubuntu|debian|linuxmint)
-            if command_exists lsb_release; then
-                dist_version="$(lsb_release --codename | cut -f2)"
-            fi
-            if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
-                dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
-            fi
-            echo "Operating System: $lsb_dist"
-            echo "Version: $dist_version"
-        ;;
-        centos|rhel|fedora|amazon)
-            if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
-                dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-            fi
-            echo "Operating System: $lsb_dist"
-            echo "Version: $dist_version"
-        ;;
-        *)
-            if command_exists lsb_release; then
-                dist_version="$(lsb_release --release | cut -f2)"
-            fi
-            if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
-                dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-            fi
-            if [ -z "$lsb_dist" ]; then
-              echo "Operating System: Unknown Linux"
-            else
-              echo "Operating System: $lsb_dist"
-            fi
-            if [ -n "$dist_version" ]; then
-              echo "Version: $dist_version"
-            fi
-        ;;
-    esac
-fi
+check_forked
+
+dist_version=""
+case "$lsb_dist" in
+    ubuntu|debian|raspbian)
+        if command_exists lsb_release; then
+            dist_version="$(lsb_release --codename | cut -f2)"
+        fi
+        if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
+            dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
+        fi
+        echo "Operating System: $lsb_dist"
+        echo "Version: $dist_version"
+    ;;
+    centos|rhel)
+        if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+            dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+        fi
+        echo "Operating System: $lsb_dist"
+        echo "Version: $dist_version"
+    ;;
+    *)
+        if command_exists lsb_release; then
+            dist_version="$(lsb_release --release | cut -f2)"
+        fi
+        if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+            dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+        fi
+        if [ -z "$lsb_dist" ]; then
+          echo "Operating System: Unknown Linux"
+        else
+          echo "Operating System: $lsb_dist"
+        fi
+        if [ -n "$dist_version" ]; then
+          echo "Version: $dist_version"
+        fi
+    ;;
+esac
 
 echo "Installing Terraform..."
 
-upstream_distro=$(get_upstream_distro)
-
-case "$upstream_distro" in
-    Ubuntu|Debian)
+case "$lsb_dist" in
+    ubuntu|debian|raspbian)
         if command_exists sudo; then
             sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
             wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
@@ -102,11 +106,11 @@ case "$upstream_distro" in
         else
             apt-get update && apt-get install -y gnupg software-properties-common
             wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-            echo "deb [signed-by=/usr/share/keyrings/hashicorp.archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+            echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
             apt-get update && apt-get install -y terraform
         fi
         ;;
-    CentOS|RHEL|Fedora|AmazonLinux)
+    centos|rhel)
         if command_exists sudo; then
             sudo yum install -y yum-utils
             sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
@@ -115,6 +119,17 @@ case "$upstream_distro" in
             yum install -y yum-utils
             yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
             yum -y install terraform
+        fi
+        ;;
+    fedora)
+        if command_exists sudo; then
+            sudo dnf install -y dnf-plugins-core
+            sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+            sudo dnf -y install terraform
+        else
+            dnf install -y dnf-plugins-core
+            dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+            dnf -y install terraform
         fi
         ;;
     *)
