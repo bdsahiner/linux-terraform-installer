64a3e6f79bb1f43f3fa43a8495b7c05201b06841
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2025-03-15 15:24:26 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,32 +3,110 @@ set -e
 
 get_distribution() {
     lsb_dist=""
-    dist_version=""
-    if [ -r /etc/upstream-release/lsb-release ]; then
-        lsb_dist="$(. /etc/upstream-release/lsb-release && echo "$DISTRIB_ID")"
-        dist_version="$(. /etc/upstream-release/lsb-release && echo "$DISTRIB_CODENAME")"
-    elif [ -r /etc/lsb-release ]; then
-        lsb_dist="$(. /etc/lsb-release && echo "$DISTRIB_ID")"
-        dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
-    elif [ -r /etc/os-release ]; then
-        . /etc/os-release
-        lsb_dist="$ID"
-        dist_version="$VERSION_CODENAME"
+    if [ -r /etc/os-release ]; then
+        lsb_dist="$(. /etc/os-release && echo "$ID")"
     fi
     echo "$lsb_dist"
 }
 
-lsb_dist=$(get_distribution)
-lsb_dist=$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
 
-# Ensure dist_version is set
-if [ -z "$dist_version" ]; then
-    if command -v lsb_release >/dev/null; then
-        dist_version=$(lsb_release -cs)
-    elif [ -f /etc/debian_version ]; then
-        dist_version=$(cut -d'.' -f1 /etc/debian_version)
+check_forked() {
+    if command_exists lsb_release; then
+        set +e
+        lsb_release -a -u > /dev/null 2>&1
+        lsb_release_exit_code=$?
+        set -e
+        if [ "$lsb_release_exit_code" = "0" ]; then
+            lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[:space:]')
+            dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[:space:]')
+        else
+            if [ -r /etc/debian_version ] && [ "$lsb_dist" != "ubuntu" ] && [ "$lsb_dist" != "raspbian" ]; then
+                if [ "$lsb_dist" = "osmc" ]; then
+                    lsb_dist=raspbian
+                else
+                    lsb_dist=debian
+                fi
+                dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+                case "$dist_version" in
+                    12) dist_version="bookworm" ;;
+                    11) dist_version="bullseye" ;;
+                    10) dist_version="buster" ;;
+                    9) dist_version="stretch" ;;
+                    8) dist_version="jessie" ;;
+                esac
+            fi
+        fi
     fi
+}
+
+is_wsl() {
+    case "$(uname -r)" in
+    *microsoft* ) true ;;
+    *Microsoft* ) true ;;
+    * ) false;;
+    esac
+}
+
+is_darwin() {
+    case "$(uname -s)" in
+    *darwin* ) true ;;
+    *Darwin* ) true ;;
+    * ) false;;
+    esac
+}
+
+lsb_dist=$( get_distribution )
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+
+if is_wsl; then
+    echo "WSL is not supported."
+    exit 1
 fi
+
+if [ -r "/etc/upstream-release/lsb-release" ]; then
+    . /etc/upstream-release/lsb-release
+    lsb_dist="$(echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]')"
+    dist_version="$DISTRIB_CODENAME"
+else
+    case "$lsb_dist" in
+        ubuntu)
+            if command_exists lsb_release; then
+                dist_version="$(lsb_release --codename | cut -f2)"
+            fi
+            if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
+                dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
+            fi
+        ;;
+        debian|raspbian)
+            dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+            case "$dist_version" in
+                12) dist_version="bookworm" ;;
+                11) dist_version="bullseye" ;;
+                10) dist_version="buster" ;;
+                9) dist_version="stretch" ;;
+                8) dist_version="jessie" ;;
+            esac
+        ;;
+        centos|rhel)
+            if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+                dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+            fi
+        ;;
+        *)
+            if command_exists lsb_release; then
+                dist_version="$(lsb_release --release | cut -f2)"
+            fi
+            if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+                dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+            fi
+        ;;
+    esac
+fi
+
+check_forked
 
 case "$lsb_dist" in
     ubuntu|debian)
@@ -40,7 +118,7 @@ case "$lsb_dist" in
         https://apt.releases.hashicorp.com $dist_version main" | \
         sudo tee /etc/apt/sources.list.d/hashicorp.list
         sudo apt update
-        sudo apt-get install terraform
+        sudo apt-get install -y terraform
         ;;
     centos|rhel)
         sudo yum install -y yum-utils
