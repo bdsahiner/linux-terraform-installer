710c1389f6bf287024cda6ee9fc50e12cd58b592
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2025-03-15 11:29:10 +0200

    Update linux-terraform-installer.sh

--- linux-terraform-installer.sh
+++ linux-terraform-installer.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
-command_exists() {
-    command -v "$@" > /dev/null 2>&1
-}
+#!/bin/sh
 
 get_distribution() {
     lsb_dist=""
@@ -12,13 +8,16 @@ get_distribution() {
     echo "$lsb_dist"
 }
 
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
 check_forked() {
     if command_exists lsb_release; then
         set +e
         lsb_release -a -u > /dev/null 2>&1
         lsb_release_exit_code=$?
         set -e
-
         if [ "$lsb_release_exit_code" = "0" ]; then
             lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[:space:]')
             dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[:space:]')
@@ -31,21 +30,49 @@ check_forked() {
                 fi
                 dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
                 case "$dist_version" in
-                    12) dist_version="bookworm";;
-                    11) dist_version="bullseye";;
-                    10) dist_version="buster";;
-                    9) dist_version="stretch";;
-                    8) dist_version="jessie";;
+                    12)
+                        dist_version="bookworm"
+                    ;;
+                    11)
+                        dist_version="bullseye"
+                    ;;
+                    10)
+                        dist_version="buster"
+                    ;;
+                    9)
+                        dist_version="stretch"
+                    ;;
+                    8)
+                        dist_version="jessie"
+                    ;;
                 esac
             fi
         fi
     fi
 }
 
-lsb_dist=$(get_distribution)
+is_wsl() {
+    case "$(uname -r)" in
+    *microsoft* ) true ;;
+    *Microsoft* ) true ;;
+    * ) false;;
+    esac
+}
+
+is_darwin() {
+    case "$(uname -s)" in
+    *darwin* ) true ;;
+    *Darwin* ) true ;;
+    * ) false;;
+    esac
+}
+
+lsb_dist=$( get_distribution )
 lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
-check_forked
+if is_wsl; then
+    exit 1
+fi
 
 case "$lsb_dist" in
     ubuntu)
@@ -55,27 +82,32 @@ case "$lsb_dist" in
         if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
             dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
         fi
-        ;;
+    ;;
     debian|raspbian)
         dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
         case "$dist_version" in
-            12) dist_version="bookworm";;
-            11) dist_version="bullseye";;
-            10) dist_version="buster";;
-            9) dist_version="stretch";;
-            8) dist_version="jessie";;
+            12)
+                dist_version="bookworm"
+            ;;
+            11)
+                dist_version="bullseye"
+            ;;
+            10)
+                dist_version="buster"
+            ;;
+            9)
+                dist_version="stretch"
+            ;;
+            8)
+                dist_version="jessie"
+            ;;
         esac
-        ;;
-    centos|rhel|amazon)
+    ;;
+    centos|rhel)
         if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
             dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
         fi
-        ;;
-    fedora)
-        if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
-            dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-        fi
-        ;;
+    ;;
     *)
         if command_exists lsb_release; then
             dist_version="$(lsb_release --release | cut -f2)"
@@ -83,70 +115,21 @@ case "$lsb_dist" in
         if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
             dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
         fi
-        ;;
+    ;;
 esac
 
-echo "Operating System: $lsb_dist"
-echo "Version: $dist_version"
-
-echo "Installing Terraform..."
+check_forked
 
 case "$lsb_dist" in
-    ubuntu|debian|raspbian)
-        if command_exists sudo; then
-            sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-            wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-            echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $dist_version main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-            sudo apt-get update && sudo apt-get install -y terraform
-        else
-            apt-get update && apt-get install -y gnupg software-properties-common
-            wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-            echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $dist_version main" | tee /etc/apt/sources.list.d/hashicorp.list
-            apt-get update && apt-get install -y terraform
-        fi
-        ;;
-    centos|rhel)
-        if command_exists sudo; then
-            sudo yum install -y yum-utils
-            sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-            sudo yum -y install terraform
-        else
-            yum install -y yum-utils
-            yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-            yum -y install terraform
-        fi
-        ;;
-    fedora)
-        if command_exists sudo; then
-            sudo dnf install -y dnf-plugins-core
-            sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-            sudo dnf -y install terraform
-        else
-            dnf install -y dnf-plugins-core
-            dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-            dnf -y install terraform
-        fi
-        ;;
-    amazon)
-        if command_exists sudo; then
-            sudo yum install -y yum-utils
-            sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-            sudo yum -y install terraform
-        else
-            yum install -y yum-utils
-            yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-            yum -y install terraform
-        fi
-        ;;
+    ubuntu|debian|raspbian|centos|fedora|rhel|sles)
+    ;;
     *)
-        echo "Terraform installation is not supported on this distribution."
+        if [ -z "$lsb_dist" ] && is_darwin; then
+            exit 1
+        fi
         exit 1
-        ;;
+    ;;
 esac
 
-if command_exists terraform; then
-    echo "Terraform has been successfully installed."
-else
-    echo "Terraform installation failed."
-    exit 1
-fi
+echo "$lsb_dist"
+echo "$dist_version"
