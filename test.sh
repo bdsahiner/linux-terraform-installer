1afca6f3d5739204cf57aa42cb766ff6857cad4d
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2025-03-15 15:01:06 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,39 +3,33 @@ set -e
 
 get_distribution() {
     lsb_dist=""
+    dist_version=""
     if [ -r /etc/upstream-release/lsb-release ]; then
         lsb_dist="$(. /etc/upstream-release/lsb-release && echo "$DISTRIB_ID")"
+        dist_version="$(. /etc/upstream-release/lsb-release && echo "$DISTRIB_CODENAME")"
     elif [ -r /etc/lsb-release ]; then
         lsb_dist="$(. /etc/lsb-release && echo "$DISTRIB_ID")"
+        dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
     elif [ -r /etc/os-release ]; then
-        lsb_dist="$(. /etc/os-release && echo "$ID")"
+        . /etc/os-release
+        lsb_dist="$ID"
+        dist_version="$VERSION_CODENAME"
     fi
     echo "$lsb_dist"
-}
-
-check_forked() {
-    if command -v lsb_release > /dev/null; then
-        set +e
-        lsb_release -a -u > /dev/null 2>&1
-        lsb_release_exit_code=$?
-        set -e
-
-        if [ "$lsb_release_exit_code" = "0" ]; then
-            lsb_dist=$(lsb_release -a -u 2>&1 | grep -E 'id' | cut -d ':' -f 2 | tr -d '[:space:]')
-            dist_version=$(lsb_release -a -u 2>&1 | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[:space:]')
-        else
-            if [ -r /etc/debian_version ] && [ "$lsb_dist" != "ubuntu" ] && [ "$lsb_dist" != "raspbian" ]; then
-                lsb_dist="debian"
-                dist_version="$(sed 's/\..*//' /etc/debian_version)"
-            fi
-        fi
-    fi
 }
 
 lsb_dist=$(get_distribution)
 lsb_dist=$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')
 
-dist_version=""
+# Ensure dist_version is set
+if [ -z "$dist_version" ]; then
+    if command -v lsb_release >/dev/null; then
+        dist_version=$(lsb_release -cs)
+    elif [ -f /etc/debian_version ]; then
+        dist_version=$(cut -d'.' -f1 /etc/debian_version)
+    fi
+fi
+
 case "$lsb_dist" in
     ubuntu|debian)
         sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
@@ -43,7 +37,7 @@ case "$lsb_dist" in
         gpg --dearmor | \
         sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
         echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-        https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+        https://apt.releases.hashicorp.com $dist_version main" | \
         sudo tee /etc/apt/sources.list.d/hashicorp.list
         sudo apt update
         sudo apt-get install terraform
@@ -60,7 +54,7 @@ case "$lsb_dist" in
         ;;
     fedora)
         sudo dnf install -y dnf-plugins-core
-        sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+        sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
         sudo dnf -y install terraform
         ;;
     *)
@@ -70,4 +64,3 @@ case "$lsb_dist" in
 esac
 
 echo "Terraform installation completed successfully."
-
